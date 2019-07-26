35f19f317f9601eb42b4eae536ca2a2205f45bdb
gmx1995 <gengminxing1995@163.com>
2019-07-26 16:37:12 +0800

    Demo file

---
+++ demo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 26 16:33:37 2019
+
+@author: GMX
+"""
+
+if __name__ == '__main__':
+    """
+    This is a test.demo for our seizure detection method.
+    """
+    print("Hello World!")
